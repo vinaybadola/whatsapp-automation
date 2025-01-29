b1b98acfe161f4119b9e611d1b420f020bd4b454
vinaybadola <vinaybadola.cs18@gmail.com>
2025-01-29 14:37:02 +0530

    remove testing code

--- src/devices/routes/device-connect-route.js
+++ src/devices/routes/device-connect-route.js
@@ -6,18 +6,18 @@ const connectController = new WhatsAppConnectController();
 import express from 'express';
 const router = express.Router();
 
-// router.post("/startSession",customAuth,connectController.startSession);
-// router.post("/send-message",customAuth,connectController.sendMessage);
-// router.post("/send-message-group",customAuth,connectController.sendMessageOnGroup);
-// router.post("/fetch-groups",customAuth,connectController.fetchGroups);
-// router.post("/fetch-group-participants",customAuth, connectController.fetchGroupParticipants);
-// router.post("/logout",customAuth,connectController.logout);
+router.post("/startSession",customAuth,connectController.startSession);
+router.post("/send-message",customAuth,connectController.sendMessage);
+router.post("/send-message-group",customAuth,connectController.sendMessageOnGroup);
+router.post("/fetch-groups",customAuth,connectController.fetchGroups);
+router.post("/fetch-group-participants",customAuth, connectController.fetchGroupParticipants);
+router.post("/logout",customAuth,connectController.logout);
 
-router.post("/startSession",connectController.startSession);
-router.post("/send-message",connectController.sendMessage);
-router.post("/send-message-group",connectController.sendMessageOnGroup);
-router.post("/fetch-groups",connectController.fetchGroups);
-router.post("/fetch-group-participants", connectController.fetchGroupParticipants);
-router.post("/logout",connectController.logout);
+// router.post("/startSession",connectController.startSession);
+// router.post("/send-message",connectController.sendMessage);
+// router.post("/send-message-group",connectController.sendMessageOnGroup);
+// router.post("/fetch-groups",connectController.fetchGroups);
+// router.post("/fetch-group-participants", connectController.fetchGroupParticipants);
+// router.post("/logout",connectController.logout);
 
 export default router;
